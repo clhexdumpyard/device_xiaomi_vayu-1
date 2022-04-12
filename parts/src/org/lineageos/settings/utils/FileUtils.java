52d3c40698d9d0f406529e0def466118bca8f4ed
CLHEX <clhexftw@protonmail.com>
2022-04-12 14:31:22 +0530

    vayu: Nuke Fps Counter Tile Service
    Signed-off-by: CLHEX <clhexftw@protonmail.com>
    
    Revert "[SQUASH] vayu: parts: introduce FPS counter QS Tile"
    
    This reverts commit d38fe7d6310b6e464557ee137cd35b2face84750.
    
    Revert "vayu: sepolicy: Allow system to read measured_fps node"
    
    This reverts commit 6798099e2c31ae8022eee9135b5b2feb3d35d144.

--- parts/src/org/lineageos/settings/utils/FileUtils.java
+++ parts/src/org/lineageos/settings/utils/FileUtils.java
@@ -35,7 +35,8 @@ public final class FileUtils {
 
     /**
      * Reads the first line of text from the given file.
-     * Reference {@link BufferedReader#readLine()} for clarification on what a line is
+     * Reference {@link BufferedReader#readLine()} for clarification on what a
+     * line is
      *
      * @return the read line contents, or null on failure
      */
